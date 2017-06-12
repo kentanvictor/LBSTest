2552afdcb57e83c6dc2fe0a41f138efe01bd2d1d
kentanvictor <2030457926@qq.com>
2017-06-12 21:04:59 +0800

    second commit

--- app/src/main/java/com/example/dell/lbstest/MainActivity.java
+++ app/src/main/java/com/example/dell/lbstest/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.dell.lbstest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public LocationClient mLocationClient;
@@ -12,6 +23,86 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListenter());
         setContentView(R.layout.activity_main);
+        positionText = (TextView) findViewById(R.id.position_text_view);
+        List<String> permissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty())
+        {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }else
+        {
+            requestLocation();
+        }
+    }
+
+    private void requestLocation() {
+        mLocationClient.start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case 1:
+                if(grantResults.length > 0)
+                {
+                    for (int result : grantResults)
+                    {
+                        if(result != PackageManager.PERMISSION_GRANTED)
+                        {
+                            Toast.makeText(this , "必須同意所有權限才能使用本程序",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                }else
+                {
+                    Toast.makeText(this,"發生未知錯誤",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    public class MyLocationListenter implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("緯度：").append(location.getLatitude()).append("\n");
+            currentPosition.append("經度：").append(location.getLongitude()).append("\n");
+            currentPosition.append("定位方式：");
+            if(location.getLocType() == BDLocation.TypeGpsLocation)
+            {
+                currentPosition.append("GPS");
+            }else if (location.getLocType() == BDLocation.TypeNetWorkLocation)
+            {
+                currentPosition.append("網絡");
+            }
+            positionText.setText(currentPosition);
+        }
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+        }
+
     }
 }
